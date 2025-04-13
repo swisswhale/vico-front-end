7792a0ca6b6141a27299e5a073d8196578ea3fd6
Ochouno2364 <ochouno2364@gmail.com>
2025-04-13 16:26:29 -0400

    navbar trouble

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,31 @@
 import { useState } from 'react'
+import { Routes, Route, useNavigate} from 'react-router'
+import { useContext, useEffect} from 'react'
+
+import { BrowserRouter } from 'react-router'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import NavBar from './components/NavBar/NavBar.jsx'
+
 
 function App() {
-  
-
   return (
     <>
-      <h1> The Visual Conversation</h1>
+  
+   {/* <NavBar /> */}
+    <h1>The Visual Conversation</h1>
+    <Routes>
+      <Route path='/' element={<h3>Home</h3>}></Route>
+      <Route path='/artcollection'>Art Gallery</Route>
+      <Route path='/artcollection/new'>Add to Collection</Route>
+      <Route path='/theconversation'>Lets Talk </Route>
+
+    </Routes>
+
+    
     </>
   )
-}
+};
 
 export default App
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Link } from 'react-router';
+import { useContext} from 'react';
+
+import { UserContext } from '../../context/UserContext'
+
+
+
+const NavBar = () => {
+
+    const [user, setUser] = useContext(UserContext);
+
+    const handleSignOut = () => {
+        localStorage.removeItem('token');
+        setUser(null);
+    }
+
+
+
+    return (
+        <nav>
+      {user ? (
+        <ul>
+          <li><Link to='/'>Home</Link></li>
+          <li><Link to='/artcollection'>Art Collection</Link></li>
+          <li><Link to='/artcollection/new'>Add To Collection</Link></li>
+          <li><Link to='' onClick={handleSignOut}>Sign Out</Link></li>
+        </ul>
+      ) : (
+        <ul>
+          <li><Link to='/sign-in'>Sign In</Link></li>
+          <li><Link to='/sign-up'>Sign Up</Link></li>
+        </ul>
+      )}
+    </nav>
+    );
+};
+
+
+export default NavBar;
