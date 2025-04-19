ee2c64ec6067c6a87a1d8e491e58356a2d02636c
Ochouno2364 <ochouno2364@gmail.com>
2025-04-19 17:18:02 -0400

    front end error is Link from react-router

--- src/components/Collection/CollectionList.jsx
+++ src/components/Collection/CollectionList.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router';
 import CreateCollectionForm from './CreateCollectionForm';
 import EditCollectionForm from './EditCollectionForm';
 import Modal from '../Modal.jsx';
