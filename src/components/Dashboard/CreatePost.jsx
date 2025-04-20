455a200368699976c9590cb91b0e57a53f1e7be3
Ochouno2364 <ochouno2364@gmail.com>
2025-04-19 21:15:27 -0400

    creatapost should have function

--- src/components/Dashboard/CreatePost.jsx
+++ src/components/Dashboard/CreatePost.jsx
@@ -2,20 +2,34 @@ import { useState } from "react";
 
 import Post from "./Post";
 
-const CreateAPost = () => {
+
+const CreateAPost = ( { addAPost }) => {
+
+    const [image, setImage] = useState('');
+    const [caption, setCaption] = useState('');
+    
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if(!image || !caption) return;
+        addAPost({caption, image, comments: []});
+        setImage('');
+        setCaption('');
+    };
 
 
     return (
-     <form>
+     <form onSubmit={handleSubmit}>
         <input 
         type="image"
         placeholder="Artwork"
-        value={imageURL}
+        value={image}
+        onChange={(event) => setImage(event.target.value)}
         />
         <input 
         type="text"
         placeholder="Why this piece ..."
         value={caption}
+        onChange={(event) => setCaption(event.target.value)}
         />
         <button type="submit">Add Post</button>
      </form>
